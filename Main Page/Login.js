b6e9b00d50ac37e121e658456a7e6ed713d07aa7
Puppy-Car <165386931+Puppy-Car@users.noreply.github.com>
2024-05-17 19:54:45 +0800

    Update Login.js

--- Main Page/Login.js
+++ Main Page/Login.js
@@ -4,6 +4,7 @@ var Password2 = ["Cat", "Meow Meow"]
 
 alert("Please Login")
 var User = prompt("Please enter your username, Meow!")
+alert(User_List.includes(User))
 if (User_List.includes(User)) {
   var Password = prompt("Please enter your password, Meow!")
   if (Password1.includes(Password) || Password2.includes(Password)) {
