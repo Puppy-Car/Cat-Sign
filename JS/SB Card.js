7c60e5092cff0e31a83408c520602bc83e4d4c9a
Puppy-Car <165386931+Puppy-Car@users.noreply.github.com>
2024-07-15 19:36:23 +0800

    Update SB Card.js

--- JS/SB Card.js
+++ JS/SB Card.js
@@ -1,5 +1,12 @@
-var User1 = ["Shit Old Man"]
-var Pass1 = ["Died"]
+function ReLoad() {
+  var ReLoad = confirm("Re-Login?");
+  if (ReLoad) {
+    location.reload();
+  }
+}
+
+var User1 = ["Shit Old Man", "Cat];
+var Pass1 = ["Died", "Meow Meow"];
 
 alert("Kill yourself Bad Guy!!!");
 alert("You can login but you will die!!!");
@@ -13,5 +20,11 @@ if (User1.includes(User)) {
   var P1 = Pass1[User1.indexof(User)];
   if (Pass == P1) {
     alert("Password Approved\n\nAccount Approved");
+  } else {
+    alert("Password Not Approved");
+    ReLoad();
   }
+} else {
+  alert("Username Not Approved");
+  ReLoad();
 }
