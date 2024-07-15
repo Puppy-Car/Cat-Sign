eb7c66ea6a9a24d6e041864ea00a6863b1c94f79
Puppy-Car <165386931+Puppy-Car@users.noreply.github.com>
2024-07-15 17:02:36 +0800

    Update Login.js

--- JS/Login.js
+++ JS/Login.js
@@ -38,7 +38,7 @@ if (UserList1.includes(User) || UserList2.includes(User)) {
   
   if (Password == P1 || Password == P2) {
     alert("Welcome, Meow!!!");
-    window.open("Enter.html");
+    window.location.href = "Enter.html";
   }
   else {
     alert("Wrong Password, Meow");
