f23216ceb46975d960932f227d2130bc4922a2ba
Puppy-Car <165386931+Puppy-Car@users.noreply.github.com>
2024-07-15 19:22:50 +0800

    Update SB Card.js

--- JS/SB Card.js
+++ JS/SB Card.js
@@ -1,1 +1,9 @@
 alert("Kill yourself Bad Guy!!!");
+alert("You can login but you will die!!!");
+
+var User = prompt("Enter Username:");
+if (User1.includes(User)) {
+  alert("Username Approved");
+  
+  var Pass = prompt("Enter Password:");
+  if (Pass 
